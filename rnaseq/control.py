ba39a4fcff63afeb5b31c2a8d3be52246537f071
Samuel Bunga <sambunga094@gmail.com>
2021-12-19 22:10:48 -0500

    add aligner and library type

--- rnaseq/control.py
+++ rnaseq/control.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     # initiate gui
     frame = set_gui()
 
-    # get input path to fastq files
+    # set input path to fastq files
     frame.set_buttons(text='Fastq files',
                       button='input',
                       cmd=frame.select_button,
@@ -18,7 +18,7 @@ if __name__ == "__main__":
                       row=1, col=1, sticky='W',
                       labels={'button': 'input', 'row': 1, 'col': 2, 'sticky': 'W'})
 
-    # get output directory
+    # set output directory
     frame.set_buttons(text='Output dir',
                       button='output',
                       cmd=frame.select_button,
@@ -26,11 +26,29 @@ if __name__ == "__main__":
                       row=2, col=1, sticky='W',
                       labels={'button': 'output', 'row': 2, 'col': 2, 'sticky': 'W'})
 
+    # set library types
+    frame.set_options(type='Library type',
+                      options=['Paired-end', 'Single-end'],
+                      row=3, col=1, sticky='W')
+
+    # set aligner
+    frame.set_options(type='Aligner to use',
+                      options=['STAR', 'salmon'],
+                      row=5, col=1, sticky='W')
+
+    # start pipeline
+    frame.set_buttons(text='Start analysis',
+                      button='start',
+                      cmd=frame.start,
+                      font='Ariel',
+                      row=8, col=1, sticky='W',
+                      labels={'button': 'start', 'row': 8, 'col': 2, 'sticky': 'E'})
+
     # exit
-    exitButton = frame.set_buttons(text='Exit',
-                                   button='quit',
-                                   cmd=frame.quit,
-                                   font='Ariel',
-                                   row=3, col=1, sticky='W',
-                                   labels={'button': 'quit', 'row': 3, 'col': 2, 'sticky': 'E'})
+    frame.set_buttons(text='Exit',
+                      button='quit',
+                      cmd=frame.quit,
+                      font='Ariel',
+                      row=8, col=2, sticky='W',
+                      labels={'button': 'quit', 'row': 9, 'col': 2, 'sticky': 'E'})
     frame.run_frame()
--- rnaseq/gui/main_frame.py
+++ rnaseq/gui/main_frame.py
@@ -4,6 +4,7 @@ import tkinter as tk
 from tkinter import font
 from tkinter import Label
 from tkinter import StringVar
+from tkinter import OptionMenu
 from tkinter import messagebox
 from tkinter import filedialog
 
@@ -17,23 +18,24 @@ class set_gui:
         self.root.geometry('550x550')  # width x height
         self.bg_color = '#d8d8d8'
         self.btn_width = 12
-        self.x_padding = (10, 5)
+        self.x_padding = (25, 0)
+        self.y_padding = (5, 10)
         self.fonts = font.Font(root=self.root, family='Arial', size=14, weight='bold')
         self.button = ''
         self.all_buttons = {}
         self.all_labels = {}
+        self.opts = {}
         # Initiate the main frame
         self.main_frame = tk.Frame(self.root, bg=self.bg_color,
-                                   padx=10, pady=10)
+                                   padx=1, pady=10)
 
     def set_buttons(self, text, button, cmd,
                     font, row, col, sticky, labels):
         if button not in self.all_buttons:
-            self.all_buttons[button] = ('', '')
+            self.all_buttons[button] = ''
 
         self.button = button
         self._initiate_label(labels)
-        print(self.all_labels)
         btn = tk.Button(self.main_frame,
                         text=text,
                         command=lambda onclick=text: cmd(onclick),
@@ -41,6 +43,18 @@ class set_gui:
                         font=font,
                         width=self.btn_width)
         btn.grid(row=row, column=col, sticky=sticky)
+
+    def set_options(self, type, options, row, col, sticky):
+        label = Label(self.main_frame, text=type,
+                      bg=self.bg_color, font=self.fonts)
+        label.grid(row=row, column=col, columnspan=1, sticky=sticky)
+        if type not in self.opts:
+            self.opts[type] = tk.StringVar(self.main_frame)
+            self.opts[type].set(options[0])
+
+        opt = OptionMenu(self.main_frame, self.opts[type], *options)
+        opt.grid(row=row+1, column=col, sticky='EW', padx=self.x_padding,
+                 pady=self.y_padding)
 
     def select_button(self, onclick):
         select_folder = filedialog.askdirectory(initialdir='.')
@@ -68,6 +82,11 @@ class set_gui:
                            sticky=labels_dict['sticky'],
                            padx=self.x_padding)
             self.all_labels[self.button] = (label, label_obj)
+
+    def start(self, labels):
+        print(self.opts['Library type'].get())
+        print(self.opts['Aligner to use'].get())
+        print(self.all_buttons)
 
     def quit(self, labels):
         self.root.quit()
